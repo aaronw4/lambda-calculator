67f032ca9ba5dd3fb60e38038d98b0c73faf15c7
Aaron Williams <aaronrw4@yahoo.com>
2019-09-20 20:35:09 -0700

    Got numbers to appear

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {numbers} from '../../../data.js';
+import {NumberButton} from './NumberButton.js';
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -7,13 +9,14 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
-  // STEP 2 - add the imported data to state
+export const Numbers = (props) => {
+  const numbersArray = numbers;  
+
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className='numbers-container'>      
+       {numbersArray.map(data => {
+      return <NumberButton buttonData = {data}/>}
+  )}
     </div>
   );
 };
